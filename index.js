edec04b05f944f1b42d57d75816c7563190c71db
ShafayetAhmad <shafayetahmad1@gmail.com>
2023-10-20 23:16:14 +0530

    user data store implemented

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 
-//middleware
-
+// middleware
 app.use(cors());
 app.use(express.json());
 
@@ -26,18 +25,45 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
+    console.log("Connected to MongoDB");
+
+    // Create a reference to the "users" collection in your database
+    const usersCollection = client.db("FlashTech").collection("users");
+
+    // Define the route to handle POST requests for adding a new user
+    app.post("/addNewUser", async (req, res) => {
+      const newUser = req.body;
+      console.log(newUser);
+
+      try {
+        // Insert the new user data into the MongoDB collection
+        const result = await usersCollection.insertOne(newUser);
+
+        // Respond with a success message
+        res.status(201).json({
+          message: "User added successfully",
+          insertedId: result.insertedId,
+        });
+      } catch (error) {
+        console.error("Error adding user to MongoDB:", error);
+        // Respond with an error message
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    // Ensure that the client will close when you finish/error
+    // await client.close();
   }
 }
+
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
