b3c19f6bcbb0818e72d23e950991c187db29f947
ShafayetAhmad <shafayetahmad1@gmail.com>
2023-10-23 17:31:51 +0530

    user cart implemented successfully

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 const ProductsData = require("./productsData");
@@ -44,17 +44,15 @@ async function run() {
       console.log(newUser);
 
       try {
-        // Insert new user data into the MongoDB collection
         const result = await usersCollection.insertOne(newUser);
 
-        // Respond with a success message
         res.status(201).json({
           message: "User added successfully",
           insertedId: result.insertedId,
         });
       } catch (error) {
         console.error("Error adding user to MongoDB:", error);
-        // Respond with an error message
+
         res.status(500).json({ error: "Internal Server Error" });
       }
     });
@@ -64,54 +62,118 @@ async function run() {
       console.log(newProduct);
 
       try {
-        // Insert new user data into the MongoDB collection
         const result = await ProductsCollection.insertOne(newProduct);
 
-        // Respond with a success message
         res.status(201).json({
           message: "Product added successfully",
           insertedId: result.insertedId,
         });
       } catch (error) {
         console.error("Error adding Product to MongoDB:", error);
-        // Respond with an error message
+
         res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
+
+    app.put("/updateUserCart", async (req, res) => {
+      console.log(req.body);
+      const { userEmail, currentProduct } = req.body;
+      console.log("line 84", userEmail, currentProduct);
+      try {
+        // Insert new user data into the MongoDB collection
+        const user = await usersCollection.findOne({
+          userEmail: userEmail,
+        });
+        console.log("90", user);
+
+        if (!user) {
+          return res
+            .status(404)
+            .json({ success: false, message: "user not found" });
+        }
+
+        const result = await usersCollection.updateOne(
+          { userEmail: userEmail },
+          { $push: { userCart: currentProduct } }
+        );
+
+        if (result.modifiedCount === 1) {
+          console.log("Product added to cart successfully");
+          res.json({
+            success: true,
+            message: "Product added to cart successfully",
+            userCart: currentProduct,
+          });
+        } else {
+          res
+            .status(500)
+            .json({ success: false, message: "Internal server error" });
+        }
+      } catch (error) {
+        console.error("Error updating user cart:", error);
+        res
+          .status(500)
+          .json({ success: false, message: "Internal server error" });
       }
     });
 
     app.get("/getBrandsData", async (req, res) => {
       const cursor = await BrandsCollection.find({}).toArray();
-      console.log(cursor);
       res.send(cursor);
     });
+
+    app.get("/getCartData", async (req, res) => {
+      try {
+        const userEmail = req.query.userEmail;
+        const user = await usersCollection.findOne({ userEmail: userEmail });
+        res.json(user.userCart);
+      } catch (error) {
+        console.error("Error fetching cart data:", error);
+        res
+          .status(500)
+          .json({ success: false, message: "Internal Server Error" });
+      }
+    });
+
     app.get("/getProductByBrand", async (req, res) => {
       const brand = req.query.brand;
-      console.log(brand);
       const cursor = await ProductsCollection.find({
         BrandName: `${brand}`,
       }).toArray();
-      console.log(cursor);
       res.send(cursor);
     });
-   const { ObjectId } = require("mongodb");
 
-   app.get("/getProductDetails", async (req, res) => {
-     const productId = req.query.id;
-     console.log(productId);
+    app.get("/getProductDetails", async (req, res) => {
+      const productId = req.query.id;
 
-     try {
-       const cursor = await ProductsCollection.findOne({
-         _id: new ObjectId(productId),
-       });
+      try {
+        const cursor = await ProductsCollection.findOne({
+          _id: new ObjectId(productId),
+        });
 
-       console.log(cursor);
-       res.send(cursor);
-     } catch (error) {
-       console.error("Error fetching product details:", error);
-       res.status(500).json({ error: "Internal Server Error" });
-     }
-   });
+        res.send(cursor);
+      } catch (error) {
+        console.error("Error fetching product details:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
 
+    app.get("/getUserDetails", async (req, res) => {
+      const userMail = req.query.userEmail;
+      console.log(userMail);
+
+      try {
+        const cursor = await usersCollection.findOne({
+          userEmail: userMail,
+        });
+
+        console.log(cursor);
+        res.send(cursor);
+      } catch (error) {
+        console.error("Error fetching product details:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
 
     app.post("/getUserByEmail", async (req, res) => {
       const email = req.body;
